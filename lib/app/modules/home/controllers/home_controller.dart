de76e4ac1cac38782cb8eca79379d2da93d7a945
akshaykumar-binSeq <akshaykumar__v_j@outlook.com>
2021-12-29 23:25:03 +0530

    API implemeneted, CRUD operations added

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,17 +1,54 @@
 import 'package:get/get.dart';
+import 'package:user_data_from_api/app/data/model/data_model.dart';
+import 'package:user_data_from_api/app/data/resources/api_provider/api_provider.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  List<UserDetails>? usersData;
+  var userDatas = [].obs;
+
+  Future getUserDataList() async {
+    List<UserDetails>? userData = await ApiProvider().getUserDataList();
+    if (userData != null) {
+      usersData = userData;
+      userDatas.value = usersData!;
+    } else {
+      print("User data is null");
+    }
+  }
+
+  addUserData(name, email, phone, gender) async {
+    Map data = {
+      "name": name,
+      "email": email,
+      "mobile": phone,
+      "gender": gender
+    };
+    await ApiProvider().createUserData(data);
+    await getUserDataList();
+  }
+
+  updataUsersData(name, email, phone, gender, id) async {
+    Map data = {
+      "name": name,
+      "email": email,
+      "mobile": phone,
+      "gender": gender
+    };
+    await ApiProvider().updateUserData(data, id);
+    await getUserDataList();
+  }
 
   final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    getUserDataList();
   }
 
   @override
   void onReady() {
     super.onReady();
+    getUserDataList();
   }
 
   @override
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,23 +1,243 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:user_data_from_api/app/data/resources/api_provider/api_provider.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  final control = Get.put(HomeController());
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneController = TextEditingController();
+  final genderController = TextEditingController();
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+
+  HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    // control.getUserDataList();
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: const Text('User Details'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: _userDataList(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          String id = "61cc6fa497069d03e8493092";
+          await ApiProvider().deleteUserData(id);
+        },
       ),
     );
+  }
+
+  _userDataList(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+          onPressed: () async {
+            _addUser(context).show();
+            await control.getUserDataList();
+          },
+          icon: const Icon(Icons.add),
+        ),
+        Obx(() => (control.userDatas.isNotEmpty)
+            ? Obx(
+                () => ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: control.userDatas.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Name: ${control.userDatas[index].name}",
+                              style: const TextStyle(color: Colors.black),
+                            ),
+                            Text("email: ${control.userDatas[index].email}"),
+                            Text("mobile: ${control.userDatas[index].mobile}"),
+                            Text("Gender: ${control.userDatas[index].gender}"),
+                            Row(
+                              children: [
+                                IconButton(
+                                    onPressed: () async {
+                                      _updateUser(context,
+                                              "${control.usersData![index].id}")
+                                          .show();
+                                      //await control.getUserDataList();
+                                    },
+                                    icon: const Icon(Icons.update)),
+                                IconButton(
+                                    onPressed: () async {
+                                      await ApiProvider().deleteUserData(
+                                          "${control.usersData![index].id}");
+                                      await control.getUserDataList();
+                                    },
+                                    icon: const Icon(Icons.delete_forever)),
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              )
+            : const Center(child: Text("Empty"))),
+      ],
+    );
+  }
+
+  Alert _addUser(BuildContext context) {
+    return Alert(
+        context: context,
+        title: "Add User Data",
+        content: Form(
+          key: _key,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.account_circle),
+                  labelText: ("Name"),
+                ),
+              ),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.mail),
+                  labelText: ("Email"),
+                ),
+              ),
+              TextFormField(
+                controller: phoneController,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.phone),
+                  labelText: ("Mobile"),
+                ),
+              ),
+              TextFormField(
+                controller: genderController,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  labelText: ("Gender"),
+                ),
+              ),
+            ],
+          ),
+        ),
+        buttons: [
+          DialogButton(
+            onPressed: () async {
+              Navigator.pop(context);
+              Alert(
+                type: AlertType.success,
+                context: context,
+                title: "User Added",
+                buttons: [
+                  DialogButton(
+                    child: const Text(
+                      "COOL",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    onPressed: () => Navigator.pop(context),
+                    width: 120,
+                  )
+                ],
+              ).show();
+              if (_key.currentState!.validate()) {
+                control.addUserData(nameController.text, emailController.text,
+                    phoneController.text, genderController.text);
+              }
+            },
+            child: const Text(
+              "Continue",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            color: Colors.red,
+          )
+        ]);
+  }
+
+  // Alert _addUser(BuildContext context) {
+  //   return
+  // }
+
+  Alert _updateUser(BuildContext context, String id) {
+    return Alert(
+        context: context,
+        title: "Update User Data",
+        content: Form(
+          key: _key,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.account_circle),
+                  labelText: ("Name"),
+                ),
+              ),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.mail),
+                  labelText: ("Email"),
+                ),
+              ),
+              TextFormField(
+                controller: phoneController,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.phone),
+                  labelText: ("Mobile"),
+                ),
+              ),
+              TextFormField(
+                controller: genderController,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  labelText: ("Gender"),
+                ),
+              ),
+            ],
+          ),
+        ),
+        buttons: [
+          DialogButton(
+            onPressed: () async {
+              Navigator.pop(context);
+              Alert(
+                type: AlertType.success,
+                context: context,
+                title: "User Updated",
+                buttons: [
+                  DialogButton(
+                    child: const Text(
+                      "COOL",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    onPressed: () => Navigator.pop(context),
+                    width: 120,
+                  )
+                ],
+              ).show();
+              if (_key.currentState!.validate()) {
+                control.updataUsersData(
+                    nameController.text,
+                    emailController.text,
+                    phoneController.text,
+                    genderController.text,
+                    id);
+              }
+              await control.getUserDataList();
+            },
+            child: const Text(
+              "Update",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            color: Colors.red,
+          )
+        ]);
   }
 }
